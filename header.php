67f8cf4b18b08ad5d5b33b81841bf005e5c35e39
deshack <mattia.mglrn@gmail.com>
2012-08-01 14:49:46 +0200

    Patch 4

--- header.php
+++ header.php
@@ -71,59 +71,59 @@
 <div id="page" class="hfeed">
 	<header id="branding" role="banner">
 			<hgroup>
-				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
-				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</hgroup>
-
-			<?php
-				// Check to see if the header image has been removed
-				$header_image = get_header_image();
-				if ( $header_image ) :
-					// Compatibility with versions of WordPress prior to 3.4.
-					if ( function_exists( 'get_custom_header' ) ) {
-						// We need to figure out what the minimum width should be for our featured image.
-						// This result would be the suggested width if the theme were to implement flexible widths.
-						$header_image_width = get_theme_support( 'custom-header', 'width' );
-					} else {
-						$header_image_width = HEADER_IMAGE_WIDTH;
-					}
-					?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 				<?php
-					// The header image
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
-							$image[1] >= $header_image_width ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else :
+					// Check to see if the header image has been removed
+					$header_image = get_header_image();
+					if ( $header_image ) :
 						// Compatibility with versions of WordPress prior to 3.4.
 						if ( function_exists( 'get_custom_header' ) ) {
-							$header_image_width  = get_custom_header()->width;
-							$header_image_height = get_custom_header()->height;
+							// We need to figure out what the minimum width should be for our featured image.
+							// This result would be the suggested width if the theme were to implement flexible widths.
+							$header_image_width = get_theme_support( 'custom-header', 'width' );
 						} else {
-							$header_image_width  = HEADER_IMAGE_WIDTH;
-							$header_image_height = HEADER_IMAGE_HEIGHT;
+							$header_image_width = HEADER_IMAGE_WIDTH;
 						}
-						?>
-					<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
-			</a>
-			<?php endif; // end check for removed header image ?>
+				?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php
+						// The header image
+						// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+						if ( is_singular() && has_post_thumbnail( $post->ID ) &&
+								( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
+								$image[1] >= $header_image_width ) :
+							// Houston, we have a new header image!
+							echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+						else :
+							// Compatibility with versions of WordPress prior to 3.4.
+							if ( function_exists( 'get_custom_header' ) ) {
+								$header_image_width  = get_custom_header()->width;
+								$header_image_height = get_custom_header()->height;
+							} else {
+								$header_image_width  = HEADER_IMAGE_WIDTH;
+								$header_image_height = HEADER_IMAGE_HEIGHT;
+							}
+							?>
+						<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
+					<?php endif; // end check for featured image or standard header ?>
+				</a>
+				<?php endif; // end check for removed header image ?>
 
-			<?php
-				// Has the text been hidden?
-				if ( 'blank' == get_header_textcolor() ) :
-			?>
-				<div class="only-search<?php if ( $header_image ) : ?> with-image<?php endif; ?>">
-				<?php get_search_form(); ?>
-				</div>
-			<?php
-				else :
-			?>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
+				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+	
+				<?php
+					// Has the text been hidden?
+					if ( 'blank' == get_header_textcolor() ) :
+				?>
+					<div class="only-search<?php if ( $header_image ) : ?> with-image<?php endif; ?>">
+					<?php get_search_form(); ?>
+					</div>
+				<?php
+					else :
+				?>
+					<?php get_search_form(); ?>
+				<?php endif; ?>
+			</hgroup>
 
 			<nav id="access" role="navigation">
 				<h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
